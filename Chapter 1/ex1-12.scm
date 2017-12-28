d689f41dc2fb3a9ed521bd02aaf48c5e91d372f4
mcrwfrd <mcrwfrd@gmail.com>
2017-12-28 13:25:03 -0800

    finish exercise 1-12

--- Chapter 1/ex1-12.scm
+++ Chapter 1/ex1-12.scm
@@ -1,6 +1,6 @@
+; Computes the entries of Pascal's triange starting from the top-most cell at r = 0, c = 0. Defensive programing in the first condition to prevent from returning false values.
 (define (pascal r c)
-  (cond ((> c r) "Value does not exist")
+  (cond ((or (> c r) (< c 0)) -1)
         ((or (= r 0) (= r 1)) 1)
         ((or (= c 0) (= c r)) 1)
         (else (+ (pascal (- r 1) c) (pascal (- r 1) (- c 1))))))
-      
