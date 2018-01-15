77c62d8beeb488a1768a4bb43f4c28ed2507825c
mcrwfrd <mcrwfrd@gmail.com>
2018-01-14 17:45:45 -0800

    finish ex 2

--- Chapter 2/ex2-01.scm
+++ Chapter 2/ex2-01.scm
@@ -1,3 +1,23 @@
+#lang sicp
+
+; Ration numbers boilerplate
+
+(define (numer rat) (car rat))
+(define (denom rat) (cdr rat))
+
+(define (print-rat rat)
+  (newline)
+  (display (numer rat))
+  (display "/")
+  (display (denom rat)))
+
+;(define (make-rat n d) (cons n d))  
+
 (define (make-rat n d)
   (cond ((< d 0) (cons (* -1 n) d))
-        (else (cons n d))
+        (else (cons n d))))
+
+(define x (make-rat 1 2))
+
+
+
---
+++ Chapter 2/ex2-01.scm~
@@ -0,0 +1,3 @@
+(define (make-rat n d)
+  (cond ((< d 0) (cons (* -1 n) d))
+        (else (cons n d))
